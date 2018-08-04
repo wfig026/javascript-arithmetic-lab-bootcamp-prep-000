e3d4c769c6b0c8053ff676bfdf696f1627979a1d
Will Figueiredo <will@wfigueiredo.com>
2018-08-04 17:20:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 function add(a, b) {
-  a = 26;
-  b = 59;
-  return a + b;
+  return a += b;
 }
 
 
 function subtract() {
-  
+  return a -= b;
 }
 
 function multiply() {
-  
+  return a *= b;
 }
 
 function divide() {
-  
+  return a /= b;
 }
 
 function inc(n) {
-  
+  return n++
 }
 
 function dec(n) {
-  
+  return n--
 }
 
 function makeInt(n) {
-  
+  return parseInt(n);
 }
 
 function preserveDecimal(n) {
-  
+  return parseFloat(n);
 }
