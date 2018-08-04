af0fa096a4206fb0ba14723e8a86532baff26e29
Will Figueiredo <will@wfigueiredo.com>
2018-08-04 17:15:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add() {
+  
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n) {
+  
+}
